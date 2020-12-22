318871e287192b93c58a1d5af0508e7d3b46ffa4
Jupiter007 <paul.kepinski@orange.fr>
2020-12-22 20:02:41 +0100

    Rename landscape file

--- build/main.js
+++ build/main.js
@@ -561,149 +561,6 @@ class Hud {
     }
 }
 
-class Viewport {
-    constructor(game) {
-        this.Game = game;
-        this.currentScene = this.Game.Overworld.getSpawnScene();
-        this.nextScene = null;
-        this.music = this.currentScene.music;
-        this.music.play();
-        this.x = 0;
-        this.y = 0;
-        this.dr = 0;
-        this.dc = 0;
-        this.slideSceneAnimationSpeed = 10;
-    }
-    get cellSize() {
-        return this.currentScene.cellSize;
-    }
-    get nbRow() {
-        return this.currentScene.nbRow;
-    }
-    get nbCol() {
-        return this.currentScene.nbCol;
-    }
-    get width() {
-        return this.currentScene.cellSize * this.currentScene.nbCol;
-    }
-    get height() {
-        return this.currentScene.cellSize * this.currentScene.nbRow;
-    }
-    loopCells(callback, scene = this.currentScene) {
-        for (let col = 0; col < this.nbCol; col++) {
-            for (let row = 0; row < this.nbRow; row++) {
-                callback(scene.getCell(col, row), col, row);
-            }
-        }
-    }
-    loopCollision(callback) {
-        this.loopCells((cell, col, row) => {
-            if (cell.brick.hasCollisions) {
-                callback(cell, col, row);
-            }
-        });
-    }
-    draw() {
-        this.loopCells((cell, col, row) => {
-            this.currentScene.drawImage(cell.brick.sprite, this.cellSize * col, this.cellSize * row, this.cellSize, this.cellSize);
-        });
-        if (this.nextScene !== null) {
-            this.loopCells((cell, col, row) => {
-                this.nextScene.drawImage(cell.brick.sprite, this.cellSize * col, this.cellSize * row, this.cellSize, this.cellSize);
-            }, this.nextScene);
-        }
-    }
-    slideSceneAnimationMove() {
-        if (this.dc === 1) {
-            this.currentScene.x -= this.slideSceneAnimationSpeed;
-            this.nextScene.x -= this.slideSceneAnimationSpeed;
-        }
-        else if (this.dc === -1) {
-            this.currentScene.x += this.slideSceneAnimationSpeed;
-            this.nextScene.x += this.slideSceneAnimationSpeed;
-        }
-        else if (this.dr === 1) {
-            this.currentScene.y -= this.slideSceneAnimationSpeed;
-            this.nextScene.y -= this.slideSceneAnimationSpeed;
-        }
-        else if (this.dr === -1) {
-            this.currentScene.y += this.slideSceneAnimationSpeed;
-            this.nextScene.y += this.slideSceneAnimationSpeed;
-        }
-        if ((this.nextScene.y <= 0 && this.dr === 1) ||
-            (this.nextScene.y >= 0 && this.dr === -1) ||
-            (this.nextScene.x <= 0 && this.dc === 1) ||
-            (this.nextScene.x >= 0 && this.dc === -1)) {
-            this.nextScene.y = 0;
-            this.nextScene.x = 0;
-            this.dr = 0;
-            this.dc = 0;
-            if (this.music.src != this.nextScene.music.src) {
-                this.music.pause();
-                this.music.currentTime = 0;
-                this.music = this.nextScene.music;
-                this.music.play();
-            }
-            this.currentScene = this.nextScene;
-            this.nextScene = null;
-            this.Game.Enemies = new Enemies(this.Game);
-            this.Game.status = GameStatus.Run;
-        }
-    }
-    collisions() {
-    }
-    drawImage(sprite, x, y, width, height) {
-        this.Game.drawImage(sprite, x + this.x, y + this.y, width, height);
-    }
-    slideScene(direction) {
-        let currentSceneCol = this.currentScene.c;
-        let currentSceneRow = this.currentScene.r;
-        if (direction === Direction.Left) {
-            this.dc = -1;
-        }
-        else if (direction === Direction.Right) {
-            this.dc = 1;
-        }
-        else if (direction === Direction.Up) {
-            this.dr = -1;
-        }
-        else if (direction === Direction.Down) {
-            this.dr = 1;
-        }
-        else {
-            return;
-        }
-        if (!(currentSceneCol + this.dc < 0 ||
-            currentSceneCol + this.dc > this.Game.Overworld.nbCol - 1 ||
-            currentSceneRow + this.dr < 0 ||
-            currentSceneRow + this.dr > this.Game.Overworld.nbRow - 1)) {
-            this.nextScene = this.Game.Overworld.map[currentSceneCol + this.dc][currentSceneRow + this.dr];
-            if (direction === Direction.Left) {
-                this.nextScene.x = -this.width;
-                this.nextScene.y = 0;
-            }
-            else if (direction === Direction.Right) {
-                this.nextScene.x = this.width;
-                this.nextScene.y = 0;
-            }
-            else if (direction === Direction.Up) {
-                this.nextScene.y = -this.height;
-                this.nextScene.x = 0;
-            }
-            else if (direction === Direction.Down) {
-                this.nextScene.y = this.height;
-                this.nextScene.x = 0;
-            }
-            this.Game.Player.dx = 0;
-            this.Game.Player.dy = 0;
-            this.Game.status = GameStatus.SlideScene;
-            return;
-        }
-        this.dc = 0;
-        this.dr = 0;
-    }
-}
-
 class Cell extends SimpleBox {
     constructor(x, y, size, brick) {
         super();
@@ -1031,6 +888,149 @@ class Sword extends SimpleBox {
     }
 }
 
+class Viewport {
+    constructor(game) {
+        this.Game = game;
+        this.currentScene = this.Game.Overworld.getSpawnScene();
+        this.nextScene = null;
+        this.music = this.currentScene.music;
+        this.music.play();
+        this.x = 0;
+        this.y = 0;
+        this.dr = 0;
+        this.dc = 0;
+        this.slideSceneAnimationSpeed = 10;
+    }
+    get cellSize() {
+        return this.currentScene.cellSize;
+    }
+    get nbRow() {
+        return this.currentScene.nbRow;
+    }
+    get nbCol() {
+        return this.currentScene.nbCol;
+    }
+    get width() {
+        return this.currentScene.cellSize * this.currentScene.nbCol;
+    }
+    get height() {
+        return this.currentScene.cellSize * this.currentScene.nbRow;
+    }
+    loopCells(callback, scene = this.currentScene) {
+        for (let col = 0; col < this.nbCol; col++) {
+            for (let row = 0; row < this.nbRow; row++) {
+                callback(scene.getCell(col, row), col, row);
+            }
+        }
+    }
+    loopCollision(callback) {
+        this.loopCells((cell, col, row) => {
+            if (cell.brick.hasCollisions) {
+                callback(cell, col, row);
+            }
+        });
+    }
+    draw() {
+        this.loopCells((cell, col, row) => {
+            this.currentScene.drawImage(cell.brick.sprite, this.cellSize * col, this.cellSize * row, this.cellSize, this.cellSize);
+        });
+        if (this.nextScene !== null) {
+            this.loopCells((cell, col, row) => {
+                this.nextScene.drawImage(cell.brick.sprite, this.cellSize * col, this.cellSize * row, this.cellSize, this.cellSize);
+            }, this.nextScene);
+        }
+    }
+    slideSceneAnimationMove() {
+        if (this.dc === 1) {
+            this.currentScene.x -= this.slideSceneAnimationSpeed;
+            this.nextScene.x -= this.slideSceneAnimationSpeed;
+        }
+        else if (this.dc === -1) {
+            this.currentScene.x += this.slideSceneAnimationSpeed;
+            this.nextScene.x += this.slideSceneAnimationSpeed;
+        }
+        else if (this.dr === 1) {
+            this.currentScene.y -= this.slideSceneAnimationSpeed;
+            this.nextScene.y -= this.slideSceneAnimationSpeed;
+        }
+        else if (this.dr === -1) {
+            this.currentScene.y += this.slideSceneAnimationSpeed;
+            this.nextScene.y += this.slideSceneAnimationSpeed;
+        }
+        if ((this.nextScene.y <= 0 && this.dr === 1) ||
+            (this.nextScene.y >= 0 && this.dr === -1) ||
+            (this.nextScene.x <= 0 && this.dc === 1) ||
+            (this.nextScene.x >= 0 && this.dc === -1)) {
+            this.nextScene.y = 0;
+            this.nextScene.x = 0;
+            this.dr = 0;
+            this.dc = 0;
+            if (this.music.src != this.nextScene.music.src) {
+                this.music.pause();
+                this.music.currentTime = 0;
+                this.music = this.nextScene.music;
+                this.music.play();
+            }
+            this.currentScene = this.nextScene;
+            this.nextScene = null;
+            this.Game.Enemies = new Enemies(this.Game);
+            this.Game.status = GameStatus.Run;
+        }
+    }
+    collisions() {
+    }
+    drawImage(sprite, x, y, width, height) {
+        this.Game.drawImage(sprite, x + this.x, y + this.y, width, height);
+    }
+    slideScene(direction) {
+        let currentSceneCol = this.currentScene.c;
+        let currentSceneRow = this.currentScene.r;
+        if (direction === Direction.Left) {
+            this.dc = -1;
+        }
+        else if (direction === Direction.Right) {
+            this.dc = 1;
+        }
+        else if (direction === Direction.Up) {
+            this.dr = -1;
+        }
+        else if (direction === Direction.Down) {
+            this.dr = 1;
+        }
+        else {
+            return;
+        }
+        if (!(currentSceneCol + this.dc < 0 ||
+            currentSceneCol + this.dc > this.Game.Overworld.nbCol - 1 ||
+            currentSceneRow + this.dr < 0 ||
+            currentSceneRow + this.dr > this.Game.Overworld.nbRow - 1)) {
+            this.nextScene = this.Game.Overworld.map[currentSceneCol + this.dc][currentSceneRow + this.dr];
+            if (direction === Direction.Left) {
+                this.nextScene.x = -this.width;
+                this.nextScene.y = 0;
+            }
+            else if (direction === Direction.Right) {
+                this.nextScene.x = this.width;
+                this.nextScene.y = 0;
+            }
+            else if (direction === Direction.Up) {
+                this.nextScene.y = -this.height;
+                this.nextScene.x = 0;
+            }
+            else if (direction === Direction.Down) {
+                this.nextScene.y = this.height;
+                this.nextScene.x = 0;
+            }
+            this.Game.Player.dx = 0;
+            this.Game.Player.dy = 0;
+            this.Game.status = GameStatus.SlideScene;
+            return;
+        }
+        this.dc = 0;
+        this.dr = 0;
+    }
+}
+
 class WinScreen {
     constructor(game) {
         this.Game = game;
@@ -1039,6 +1039,6 @@ class WinScreen {
     draw() {
         this.music.play();
         this.Game.fillRect(0, 0, this.Game.Canvas.width, this.Game.Canvas.height, "#000");
-        this.Game.fillText("YOU WIN", this.Game.Canvas.width / 2, this.Game.Canvas.height / 2, '#fff', '24px', 'center', 'middle');
+        this.Game.fillText("YOU WON", this.Game.Canvas.width / 2, this.Game.Canvas.height / 2, '#fff', '24px', 'center', 'middle');
     }
 }
