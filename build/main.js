40bba344c27bc807cfbe845e041f4526e4d33879
Jupiter007 <paul.kepinski@orange.fr>
2021-01-02 17:53:08 +0100

    Re: fix enemies damage

--- build/main.js
+++ build/main.js
@@ -284,7 +284,7 @@ class Enemies {
     collisions() {
         this.loopEnemies((enemy) => {
             if (movingBoxsCollision(this.Game.Player.hitBox, enemy)) {
-                this.Game.Player.takeDamage(10);
+                this.Game.Player.takeDamage(1);
             }
             if (movingBoxCanvasCollision(enemy, this.Game.Viewport)) {
                 enemy.invertDirection();
