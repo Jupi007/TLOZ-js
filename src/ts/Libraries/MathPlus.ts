2ad2b8c175cd80965ef3a5e01289611c83cffac3
Jupiter007 <paul.kepinski@orange.fr>
2021-03-14 18:19:49 +0100

    Add MathPlus Librarie

---
+++ src/ts/Libraries/MathPlus.ts
@@ -0,0 +1,13 @@
+export namespace MathPlus {
+    export function trunc(number: number, nbDecimals: number = 0) {
+        nbDecimals = Math.trunc(nbDecimals);
+
+        return Math.trunc(number * (10 ** nbDecimals)) / (10 ** nbDecimals);
+    }
+
+    export function round(number: number, nbDecimals: number = 0) {
+        nbDecimals = Math.trunc(nbDecimals);
+
+        return Math.round(number * (10 ** nbDecimals)) / (10 ** nbDecimals);
+    }
+}
---
+++ test/mathplus.test.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+import { MathPlus } from "../build/Libraries/MathPlus.js";
+
+describe('Test MathPlus.trunc()', () => {
+    it('should truncate given number at given decimal', () => {
+        assert.strictEqual(MathPlus.trunc(1.123, 0), 1);
+        assert.strictEqual(MathPlus.trunc(1.123, 2), 1.12);
+        assert.strictEqual(MathPlus.trunc(1.123, 5), 1.123);
+    });
+});
+
+describe('Test MathPlus.round()', () => {
+    it('should round given number at given decimal', () => {
+        assert.strictEqual(MathPlus.round(1.53, 0), 2);
+        assert.strictEqual(MathPlus.round(1.53, 1), 1.5);
+        assert.strictEqual(MathPlus.round(1.499, 5), 1.499);
+    });
+});
